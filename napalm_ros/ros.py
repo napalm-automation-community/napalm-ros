eddbfa784202ec6441122a4d41a762b124bd5f7f
Łukasz Kostka <lukasz.kostka@netng.pl>
2016-11-07 22:34:46 +0100

    First POC working version

--- napalm_ros/ros.py
+++ napalm_ros/ros.py
@@ -1,6 +1,7 @@
 """NAPALM driver for Mikrotik RouterBoard OS (ROS)"""
 
 from napalm_base.base import NetworkDriver
+from rosapi import connect
 
 
 class ROSDriver(NetworkDriver):
@@ -17,48 +18,70 @@ class ROSDriver(NetworkDriver):
         self.password = password
         self.timeout = timeout
         optional_args = optional_args or {}
-        self.port = optional_args.get('port', 22)
-        self.ros_version = None
-        self.candidate_config = dict()
-
-    def cli(self, *commands):
-        pass
+        self.port = optional_args.get('port', 8728)
+        self.api = None
 
     def close(self):
-        pass
+        self.api.close()
 
     def open(self):
-        pass
-
-    def commit_config(self):
-        pass
-
-    def discard_config(self):
-        self.candidate_config = dict()
-
-    def get_arp_table(self):
-        # {
-        #     'interface': unicode(arp_entry.get('interface')),
-        #     'mac': napalm_base.helpers.mac(arp_entry.get('mac-address')),
-        #     'ip': napalm_base.helpers.ip(arp_entry.get('address')),
-        #     'age': float(-1),
-        # }
-        pass
-
-    def get_config(self):
-        pass
+        self.api = connect(
+                host=self.hostname,
+                username=self.username,
+                password=self.password,
+                port=self.port,
+                timeout=self.timeout
+                )
 
     def get_facts(self):
-        # return {
-        #     'uptime': ros_utils.to_seconds(system_resource['uptime']),
-        #     'vendor': unicode(system_resource['platform']),
-        #     'model': unicode(system_resource['board-name']),
-        #     'hostname': unicode(system_identity['name']),
-        #     'fqdn': u'',
-        #     'os_version': unicode(system_resource['version']),
-        #     'serial_number': unicode(system_routerboard.get('serial-number', '')),
-        #     'interface_list': napalm_base.utils.string_parsers.sorted_nicely(
-        #         [intf.get('name') for intf in self._api_get('/interface')]
-        #     ),
-        # }
-        pass
+        resource = self.api('/system/resource/print')[0]
+        identity = self.api('/system/identity/print')[0]
+        routerboard = self.api('/system/routerboard/print')[0]
+        interfaces = self.api('/interface/print')
+        return {
+            'uptime': to_seconds(resource['uptime']),
+            'vendor': resource['platform'],
+            'model': resource['board-name'],
+            'hostname': identity['name'],
+            'fqdn': u'',
+            'os_version': resource['version'],
+            'serial_number': routerboard.get('serial_number', ''),
+            'interface_list': tuple(iface['name'] for iface in interfaces)
+        }
+
+
+def to_seconds(time_format):
+    """
+    Convert time in human readable form to seconds.
+
+    :param str time_format: Time format eg. 1h22m13s
+    :returns: Converted time in seconds
+    :rtype: int
+    """
+    seconds = minutes = hours = days = weeks = 0
+
+    number_buffer = ''
+    for current_character in time_format:
+        if current_character.isdigit():
+            number_buffer += current_character
+            continue
+        if current_character == 's':
+            seconds = int(number_buffer)
+        elif current_character == 'm':
+            minutes = int(number_buffer)
+        elif current_character == 'h':
+            hours = int(number_buffer)
+        elif current_character == 'd':
+            days = int(number_buffer)
+        elif current_character == 'w':
+            weeks = int(number_buffer)
+        else:
+            raise ValueError('Invalid specifier - [{}]'.format(current_character))
+        number_buffer = ''
+
+    seconds += (minutes * 60)
+    seconds += (hours * 3600)
+    seconds += (days * 86400)
+    seconds += (weeks * 604800)
+
+    return seconds

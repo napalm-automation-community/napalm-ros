18e8448fbfc2bc36165cacd5143e2be12bb611cb
Matt Ryan <inetuid@gmail.com>
2016-12-12 10:33:01 +0000

    Replace to_seconds() with napalm_base.utils.string_parsers.convert_uptime_string_seconds().

--- napalm_ros/utils.py
+++ napalm_ros/utils.py
@@ -7,6 +7,7 @@ def export_concat(export_config):
 
     concat_config = []
     for line in export_config:
+        line = line.rstrip('\r\n')
         if line == '':
             continue
         if line[0] == '/':
@@ -15,7 +16,10 @@ def export_concat(export_config):
         elif line[0] == '#':
             continue
         elif line[-1:] == '\\':
-            line_buffer.append(line[:-1].lstrip())
+            line = line[:-1].lstrip()
+            if line[:2] == '\\_':
+                line = ' {}'.format(line[2:])
+            line_buffer.append(line)
         else:
             if export_section is None:
                 raise ValueError('No configuration section')
@@ -178,8 +182,12 @@ def print_to_values_structured(print_output):
         to_values_structured.append(key_value)
     return to_values_structured
 
+import napalm_base.utils.string_parsers
 
 def to_seconds(time_format):
+    return napalm_base.utils.string_parsers.convert_uptime_string_seconds(time_format)
+
+def Xto_seconds(time_format):
     seconds = minutes = hours = days = weeks = 0
 
     number_buffer = ''

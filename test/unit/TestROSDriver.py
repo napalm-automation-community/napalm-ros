91923f09a616dbe1aa7cd56057afb5af560e8d69
Łukasz Kostka <lukasz.kostka@netng.pl>
2017-03-01 20:16:05 +0100

    setUpClass credentials

--- test/unit/TestROSDriver.py
+++ test/unit/TestROSDriver.py
@@ -5,17 +5,15 @@ from napalm_base.test.base import TestConfigNetworkDriver, TestGettersNetworkDri
 
 
 class TestConfigROSDriver(unittest.TestCase, TestConfigNetworkDriver):
+
     @classmethod
     def setUpClass(cls):
         """Executed when the class is instantiated."""
         cls.vendor = 'ros'
         cls.device = ros.ROSDriver(
             '127.0.0.1',
-            'vagrant',
-            'vagrant',
-            timeout=60,
-            optional_args={
-                'port': 12205,
-            }
+            'admin',
+            '',
+            timeout=1,
         )
         cls.device.open()

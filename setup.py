f222f600bea5275c3981626f050af716fc76b367
Łukasz Kostka <lukasz.kostka@netng.pl>
2018-06-07 16:49:03 +0200

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def parse_reqs(file_path):
 
 setup(
     name="napalm-ros",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages(),
     author="Matt Ryan",
     author_email="inetuid@gmail.com",

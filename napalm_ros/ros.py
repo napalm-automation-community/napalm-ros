645489eb58d44b2de9f7403df884405b19f4577c
David Mc Ken <david.mcken@gmail.com>
2020-07-11 13:31:14 -0400

    Default untagged to vlan one not zero.

--- napalm_ros/ros.py
+++ napalm_ros/ros.py
@@ -92,7 +92,7 @@ class ROSDriver(NetworkDriver):
                 dict(
                     mac=entry['mac-address'],
                     interface=entry['interface'],
-                    vlan=entry.get('vid', 0),     # The vid is not consistently set in the API
+                    vlan=entry.get('vid', 1),     # The vid is not consistently set in the API
                     static=not entry['dynamic'],
                     active=not entry['invalid'],
                     moves=0,

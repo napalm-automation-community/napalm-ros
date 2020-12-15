c74635f89590a13f74c04ee3a5003cdb1d747eb9
Łukasz Kostka <lukasz.kostka@netng.pl>
2020-12-15 20:53:11 +0100

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="napalm-ros",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     author="Łukasz Kostka",
     author_email="lukasz.kostka@netng.pl",

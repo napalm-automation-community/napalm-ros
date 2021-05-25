8674f804217144106c537119ccf275eed12d96cb
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2021-05-25 20:58:19 +0200

    Add LLDPInterfaces tests

--- tests/unit/test_getters.py
+++ tests/unit/test_getters.py
@@ -1,6 +1,9 @@
 """Tests for getters."""
 
 from napalm.base.test.getters import BaseTestGetters
+from napalm_ros.ros import (
+    LLDPInterfaces,
+)
 
 import pytest
 
@@ -8,3 +11,13 @@ import pytest
 @pytest.mark.usefixtures("set_device_parameters")
 class TestGetter(BaseTestGetters):
     """Test get_* methods."""
+
+
+def test_LLDPInterfaces_obj_str():
+    obj = LLDPInterfaces(parent='bridge0', child='ether1')
+    assert str(obj) == 'bridge0/ether1'
+
+
+def test_LLDPInterfaces_fromAPI():
+    obj = LLDPInterfaces.fromApi('ether1,bridge0')
+    assert str(obj) == 'bridge0/ether1'
